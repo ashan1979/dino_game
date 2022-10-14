3d6588251b00aeda7c906d551369a35fec5193ca
ashan <indrajee79@gmail.com>
2022-10-14 14:40:12 +0530

    ground speed and score

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
 const SPEED_SCALE_INCREASE = 0.00001
 
-const worldElem = document.querySelector('[data-world')
+const worldElem = document.querySelector('[data-world]')
+const scoreElem = document.querySelector('[data-score]')
+const startScreenElem = document.querySelector('[data-start-screen]')
 
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
@@ -13,6 +15,7 @@ document.addEventListener("keydown", handleStart, { once: true})
 
 let lastTime
 let speedScale
+let score
 function update(time) {
     if (lastTime == null) {
         lastTime = time
@@ -23,6 +26,7 @@ function update(time) {
 
     updateGround(delta, speedScale)
     updateSpeedScale(delta)
+    updateScore(delta)
 
     lastTime = time
     window.requestAnimationFrame(update)
@@ -32,10 +36,17 @@ function updateSpeedScale(delta) {
     speedScale += delta * SPEED_SCALE_INCREASE
 }
 
+function updateScore(delta) {
+    score += delta * 0.01
+    scoreElem.textContent = Math.floor()
+}
+
 function handleStart () {
     lastTime = null
     speedScale = 1
+    score = 0
     setUpGround()
+    startScreenElem.classList.add("hide")
     window.requestAnimationFrame(update)
 }
 
